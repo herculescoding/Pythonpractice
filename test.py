88a07c70d16f25b4180c15593730618c99f86a00
Leman <Leman@MacBook-Pro-Leman.local>
2018-12-19 12:42:27 +0200

    Added modification code

--- test.py
+++ test.py
@@ -1,1 +1,248 @@
-print ("Im"+"years old")
+# print(100/3)
+# print(5**2)
+# print((1+3)**2)
+# print(20//6)
+# print(20%6)
+# print (7%(5//2))
+
+# print('Hello \nworld')
+
+# print ('''Hello
+# world''')
+
+# print ( input('How is your name? :'))
+
+# test=17
+
+# print(test)
+
+# a= input ('Input first number:')
+# b= input ('Input second number:')
+# print( int(a) + int(b))
+# print (int(a) - int (b) )
+# print ( int(a) / int(b))
+# print (int (a) * int(b))
+
+
+# print('One row' + 'Second row')
+
+# name = input('Your name?:')
+# print( 'Your name' + name)
+
+
+# print('Hello' * 1001)
+# name= input ('Fill up your name:')
+# count= input('How many times multi?:')
+
+# print( name * int(count))
+
+# print(4*'2')
+
+# print("Azazaza" * 4)
+
+# test='Test row'
+# test2= 2
+# print(test+str(2))
+
+# print (int("3"+"4"))
+
+
+
+# def arithmetic (arg1,arg2,op)
+# if op == '+'
+# return arg1 + arg2
+# elif op == '-'
+# return arg1 - arg2
+
+
+
+#print(int("3" + "4"))
+#print (float("210" * int("2")))
+#spam = 2
+#eggs = 3
+#del spam
+#eggs = 4
+#spam = 5
+#print(spam*eggs)
+
+
+#test=10
+#test+= 10
+#print(test)
+
+
+#x='Test'
+#x*=5
+#print(x)
+
+
+
+#test = True
+#test2 = False
+
+#print (10 == 11)
+#print (10 == 10)
+#print (10 != 10)
+#print (10 != 11)
+#print (10 > 5)
+#print (10 < 5)
+#print (7 > 7.0)
+#print ("Test" > "Tesa")
+#print (8.7 <= 8.70)
+
+
+#wether = 'Winter'
+#time = 'Day'
+
+#if wether == 'Winter' :
+    #print('Now is cold, dont go anywere')
+    #if time == 'Night' :
+        #print('Its night outside')
+#if wether == 'Rain' :
+    #print ('You gonna wett')
+
+
+#time = 'Night'
+
+#if time == 'Night' :
+    #print('Its night')
+#elif time == 'Morning' :
+    #print ('Did you eat?')
+#else:
+    #print('You can go')
+
+#password = input ('Enter pass:')
+
+#if password == '1234':
+    #print('you can go')
+#else:
+    #print('Error')
+
+
+#weather = 'Rain'
+#time = 'Day'
+
+#if weather == 'Rain' and time == 'Night' :
+    #print('any text')
+
+#weather = 'B'
+
+#if not weather == 'Rain':
+    #print('any text')
+
+
+
+
+ #http://www.itmathrepetitor.ru/prog/zadachi-na-vychisleniya/
+ #Работа с консолью
+
+    # 1.Вывести на экран текст "Silence is golden".
+#print ('Silence is golden')
+
+    #2.Вывести на экран текущее название дня недели, название месяца и свое имя. Каждое слово должно быть в отдельной строке.
+#print('Wednesday')
+#print('December')
+#print('Eugen')
+
+#3 Вывести на экран пять строк из нулей, причем количество нулей в каждой строке равно номеру строки.
+
+#print ('0')
+#print ('0' + '0')
+#print ('0' + '0' + '0')
+#print ('0' + '0' + '0' + '0')
+#print ('0' * 5)
+
+#4 Вывести на экран прямоугольник, заполненный буквами А. Количество строк в прямоугольнике равно 5, количество столбцов равно 8.
+
+
+#print ('A' * 8)
+#print ('A' * 8)
+#print ('A' * 8)
+#print ('A' * 8)
+#print ('A' * 8)
+
+
+#5 Вывести на экран букву "W" из символов "*" (звездочка).
+
+
+#print ('*' '       *' '          *')
+#print (' *' '     *' ' *' '       *')
+#print ('  *' '   *' '   *' '     *')
+#print ('   *' ' *' '     *' '  *')
+#print ('    *' '       *')
+
+#6  Вывести на экран результат вычисления 1+2−4.
+
+#print (1 + 2 - 4)
+
+#7 Вычислите 1/2+1/4. Ответ: 0.75
+
+#print (1/2 + 1/4)
+
+#8 Вычислите значение выражения (a+4b)(a−3b)+a2 при a=2 и b=3. Ответ:  -94
+
+#a = 2
+#b = 3
+
+#print (( a + 4*b ) * ( a - 3*b ) + a*2)
+
+
+#9 Вычислите |x|+x5, если x=−2. Ответ: -30
+
+#x = -2
+
+#print (abs(x) + x** (5))
+
+
+#10 Вычислите значение выражения (x+1)2+3(x+1) при а) x=1.7; б) x=3. Ответ: а) 15.39 б) 28
+
+#x = 1.7
+#x = 3
+
+#print (((x + 1) * 4) + (x + 1) * 3)
+
+
+#11 Вычислите значение выражения |x−5|−sinx3+x2+2014‾‾‾‾‾‾‾‾‾‾√cos2x−3 при x=−2.34.
+
+#x = (- 2.34)
+
+#print (abs(x - 5))
+#import math
+#print (math.sin(x))
+
+#print ((((abs(x - 5) - math.sin(x)) / (3)) + (x * 2 / 2) + 2014 / 2))
+
+#print (math.cos (x))
+
+#print (((((abs(x - 5) - math.sin(x)) / (3)) + (x * 2 / 2) + 2014 / 2)) + (math.cos (x)))
+
+
+#12 Вычислите значение выражения ex−2+|sin(x)|−x4⋅cos1x при x=3.6 Ответ: -156.1276
+
+#13 Вычислите значение выражения x2+b‾‾‾‾‾‾√5−b2sin3(x+a)x при a=0.1, b=0.2 и x=1 Ответ: 1.0088
+
+#14 Пользователь вводит два числа. Найдите сумму и произведение данных чисел.
+
+
+#a= input ('Input first number:')
+#b= input ('Input second number:')
+#print( int(a) + int(b))
+#print (int (a) * int(b))
+
+#15 Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого числа.
+
+#a= input ('Input first number:')
+#print( int(a) * 2)
+#b= input ('Input second number:')
+#print(int(a) ** 6 )
+
+#16 Пользователь вводит три числа. Увеличьте первое число в два раза, второе числа уменьшите на 3, третье число возведите в квадрат и затем найдите сумму новых трех чисел.
+
+a= input ('Input first number:')
+b= input ('Input second number:')
+c= input ('Input third number:')
+
+print(int(a) * 2)
+print(int(b) / 3)
+print(int(c) * 2)
+print()
